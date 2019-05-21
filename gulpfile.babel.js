0149c0bce11eb69383cd94b4d93e79194a8b9b83
noth8 <discryer@gmail.com>
2019-05-21 20:00:07 +0200

    Add helper functions checkDirExist, errorHandler and printFilename

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,9 @@
+import fs from "fs";
 import del from "del";
 import gulp from "gulp";
+import debug from "gulp-debug";
+import plumber from "gulp-plumber";
+import notifier from "node-notifier";
 
 
 const paths = {
@@ -11,6 +15,19 @@ const paths = {
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
 const isProduction = !isDevelopment;
+
+const checkDirExist = (location, callback) => {
+  fs.access(location, fs.constants.F_OK, callback);
+};
+
+const errorHandler = taskName => plumber({
+  errorHandler: error => notifier.notify({
+    title: `[${taskName}] Error : `,
+    message: error.message,
+  }),
+});
+
+const printFileName = taskname => debug({ title: `[${taskname}] Add : ` });
 
 function cleanBuildDir(finishTask) {
   if (isProduction) return del(paths.build.root);
